8cd9b6c0cbec320ad98f7f0e73ed95479e0ffdb0
wjadvos <wouter.devos@ugent.be>
2021-08-10 16:28:04 +0200

    added makeAllUnzipped.m

---
+++ cleanfiles/makeAllUnzipped.m
@@ -0,0 +1,18 @@
+function files=makeAllUnzipped(files)
+% this function makes sure all files are unzipped; so it checks if it is
+% the case and if it is not, does the unzipping (and returns the same
+% filelist , but then with only unzipped files)
+
+% vier situaties: filename ends with .gz or not
+% unzipped file exists or not
+% (when the filename does not end with .gz, the unzipped file exists
+% automatically, so then we don't have to do anything)
+
+    parfor i=1:length(files)
+        
+        
+        
+        
+    end
+end
+
